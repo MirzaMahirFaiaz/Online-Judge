505b79e85b97040bf96d05052b48859d54d68256
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-07-24 23:05:26 +0600

    Create A - Blood Pressure.cpp

---
+++ Atcoder/ABC211/A - Blood Pressure.cpp
@@ -0,0 +1,25 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,a,b;
+    double c;
+
+    cin>>a>>b;
+
+    c= ((double)(a-b)/3.0)+b;
+    printf("%lf",c);
+
+
+
+return 0;
+}
