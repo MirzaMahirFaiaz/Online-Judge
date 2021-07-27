89ad41c37865a8a8cd509c74f7cedd9ad8c4aa98
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-07-28 00:35:22 +0600

    Create Positive Negative Sign.cpp

---
+++ LightOJ/Warm-Up/Positive Negative Sign.cpp
@@ -0,0 +1,20 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+    ll n,t,i,j,k,m;
+
+    cin>>t;
+
+    for(i=1;i<=t;i++){
+        cin>>n>>m;
+        cout<<"Case "<<i<<": "<<n*m<<endl;
+    }
+
+return 0;
+}
