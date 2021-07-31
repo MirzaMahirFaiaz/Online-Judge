aa63a3b76649949ba7ab603f8cd2a6d417cdc5c4
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-07-31 20:31:25 +0600

    Create A - Alloy.cpp

---
+++ Atcoder/ABC212/A - Alloy.cpp
@@ -0,0 +1,27 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+    cin>>i>>j;
+
+    if(i==0)
+        cout<<"Silver"<<endl;
+    else if(j==0)
+        cout<<"Gold"<<endl;
+    else
+        cout<<"Alloy"<<endl;
+
+
+
+return 0;
+}
