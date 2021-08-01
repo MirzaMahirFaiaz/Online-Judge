aecd1e2fdea407c65d3b91a75d65f0c172657752
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-08-01 23:58:20 +0600

    Add files via upload

---
+++ LightOJ/Warm-Up/Unlucky Bird.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    double v1,v2,toron1,toron2,v3,somoy,somoy1,somoy2,durutto,ans;
+	int  t,cs;
+
+	cin>>t;
+
+	for (cs = 1; cs <= t; cs++) {
+            cin>>v1>>v2>>v3>>toron1>>toron2;
+
+		somoy1 = v1 / toron1;
+		somoy2 = v2 / toron2;
+
+		somoy = max(somoy1, somoy2);
+
+
+		ans = somoy * v3;
+		durutto = v1 * somoy1 + 0.5 *( -toron1 * somoy1 * somoy1);
+		durutto += v2 *somoy2 + 0.5 * (-toron2 * somoy2 * somoy2);
+		std::cout << std::setprecision(9) << std::fixed;
+
+		cout<<"Case "<<cs<<": "<<durutto<<" "<<ans<<endl;
+
+	}
+
+	return 0;
+}
