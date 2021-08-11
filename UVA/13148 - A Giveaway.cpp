ee1e744f854d9039a1ed006164e6a82b162b0bf4
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-08-12 00:05:51 +0600

    Create 13148 - A Giveaway.cpp

---
+++ UVA/13148 - A Giveaway.cpp
@@ -0,0 +1,24 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,n;
+    while(cin>>n){
+        if(n==0)
+            return 0;
+        if(n==1||n==64||n==729||n==4096||n==15625||n==46656||n==117649||n==262144||n==531441||n==1000000||n==1771561||n==2985984||n==4826809||n==7529536||n==11390625||n==16777216||n==24137569||n==34012224||n==47045881||n==64000000||n==85766121.)
+    cout<<"Special"<<endl;
+    else
+        cout<<"Ordinary"<<endl;
+    }
+return 0;
+}
