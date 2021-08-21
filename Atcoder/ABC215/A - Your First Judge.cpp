8ead0b7395e65c7ff7cf0fcc9b05fcaeb1b2b161
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-08-21 18:11:47 +0600

    Create A - Your First Judge.cpp

---
+++ Atcoder/ABC215/A - Your First Judge.cpp
@@ -0,0 +1,23 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+    string s="Hello,World!",s1;
+    cin>>s1;
+    if(s1==s)
+        cout<<"AC"<<endl;
+    else
+        cout<<"WA"<<endl;
+
+return 0;
+}
