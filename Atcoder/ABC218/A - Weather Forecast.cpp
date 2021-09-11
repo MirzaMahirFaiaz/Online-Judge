0066025a7efd1b626b738b16a7a814d012e95bdd
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-09-11 19:06:55 +0600

    Create A - Weather Forecast.cpp

---
+++ Atcoder/ABC218/A - Weather Forecast.cpp
@@ -0,0 +1,27 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+    string s;
+    cin>>j;
+    cin>>s;
+
+    if(s[j-1]=='o')
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+
+
+
+return 0;
+}
