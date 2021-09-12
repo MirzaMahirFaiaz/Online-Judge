42c94bf5e39a520d09d92b176989b0e77ca1be9f
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-09-12 22:03:53 +0600

    Add files via upload

---
+++ UVA/1124 - Celebrity jeopardy.cpp
@@ -0,0 +1,22 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+    string s;
+
+    while(getline(cin,s)){
+        cout<<s<<endl;
+    }
+
+return 0;
+}
