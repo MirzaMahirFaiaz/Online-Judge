fab2b78be42d56a5362b3d50a565e7dcccb96c74
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-09-29 01:10:18 +0600

    Add files via upload

---
+++ UVA/10469 - To Carry or not to Carry.cpp
@@ -0,0 +1,19 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,a,b;
+    while(cin>>a>>b){
+        cout<<(a^b)<<endl;
+    }
+return 0;
+}
