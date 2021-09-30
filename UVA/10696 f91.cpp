7b40d34b972360a2128a1d6b5e250796d8edad95
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-09-30 22:21:01 +0600

    Add files via upload

---
+++ UVA/10696 f91.cpp
@@ -0,0 +1,36 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+//ll f91(ll n){
+//    if(n<=100)
+//      return  f91(f91(n+11));
+//
+//      return n-10;
+//}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+
+    while(1){
+        cin>>t;
+        if(t==0){
+            break;
+        }
+        //cout<<"f91("<<t<<") = "<<f91(t)<<endl;
+        if(t>100)
+            cout<<"f91("<<t<<") = "<<t-10<<endl;
+        else
+            cout<<"f91("<<t<<") = 91"<<endl;
+    }
+
+return 0;
+}
