e2a34f9f22c1894c40f9e900c9a3cf4f41c45fb3
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-10-02 20:08:49 +0600

    Create A - Seismic magnitude scales.cpp

---
+++ Atcoder/ABC221/A - Seismic magnitude scales.cpp
@@ -0,0 +1,21 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+    cin>>i>>j;
+    i= abs(i-j);
+    i= pow(32,i);
+    cout<<i<<endl;
+
+return 0;
+}
