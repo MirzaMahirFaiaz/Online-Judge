e3b5a4922e3db69c45e37dcac21edbdc4aa3c95f
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-10-18 02:42:47 +0600

    Create A - Exact Price.cpp

---
+++ Atcoder/ABC 223/A - Exact Price.cpp
@@ -0,0 +1,22 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,n;
+    cin>>n;
+    if(n%100==0&&n!=0)
+        cout<<"Yes"<<endl;
+    else
+        cout<<"No"<<endl;
+
+return 0;
+}
