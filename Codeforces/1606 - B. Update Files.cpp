eb3c6e26ad05a999aefaf2e58c8159d847192e7b
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-10-29 21:41:15 +0600

    Create 1606 - B. Update Files.cpp

---
+++ Codeforces/1606 - B. Update Files.cpp
@@ -0,0 +1,36 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,n,k,noc,h,tot;
+    cin>>t;
+    while(t--){
+            noc=1,h=0;
+        cin>>n>>k;
+        while(noc<=k&&noc<n){
+           noc*=2;
+           h++;
+    }
+    if(noc>n)
+        noc=n;
+    n=n-noc;
+    tot=(n/k);
+    h+=tot;
+    if(n!=tot*k)
+        h++;
+
+    cout<<h<<endl;
+
+    }
+
+return 0;
+}
