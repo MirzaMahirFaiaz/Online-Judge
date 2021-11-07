a883a19ab2c016d4d79472a5d5a7572f18d06014
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-11-07 23:04:08 +0600

    Create 374 - Big Mod.cpp

---
+++ UVA/374 - Big Mod.cpp
@@ -0,0 +1,38 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+ll bigmodRecur ( ll b, ll p, ll m )
+{
+    if (p==0) 
+        return 1%m;
+
+    if ( p%2 == 0 )
+    {
+        ll y = bigmodRecur ( b, p / 2, m );
+        return ( y * y ) % m;
+    }
+    else
+    {
+        return ( b*bigmodRecur(b, p - 1, m) ) % m;
+    }
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,b,p,m;
+    while (cin>>b>>p>>m)
+    {
+        ll res = bigmodRecur(b,p,m);
+        cout<<res<<endl;;
+    }
+
+    return 0;
+}
