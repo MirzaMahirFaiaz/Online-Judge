3ebfdfb161a00c9c5011ec1ce92de19385f0fba8
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-11-10 19:19:51 +0600

    Create RSIGNS - Road Signs.cpp

---
+++ CodeChef/RSIGNS - Road Signs.cpp
@@ -0,0 +1,35 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+#define MOD 1000000007
+using namespace std;
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+      ll  a=10,r=2;
+	cout<<((a%MOD)*binpow(r,n-1,MOD))%MOD<<endl;
+    }
+
+return 0;
+}
