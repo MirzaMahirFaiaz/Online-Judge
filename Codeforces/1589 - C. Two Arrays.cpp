bba80943e1f10d4945b77ce96587e36aff8b2930
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-11-14 13:52:29 +0600

    Create 1589 - C. Two Arrays.cpp

---
+++ Codeforces/1589 - C. Two Arrays.cpp
@@ -0,0 +1,49 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,n,in;
+   vector<int>a,b;
+
+    cin>>t;
+    while(t--){
+            cin>>n;
+
+        for(i=0; i<n; i++){
+                cin>>in;
+            a.push_back(in);
+        }
+        for(i=0; i<n; i++){
+           cin>>in;
+            b.push_back(in);
+        }
+        ll flag=0;
+        sort(a.begin(),a.end());
+         sort(b.begin(),b.end());
+
+    for(i=0;i<n;i++){
+            if(abs(a[i] - b[i])>1){
+                flag=1;
+                break;
+            }
+    }
+    if(flag==0)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
+    a.clear();
+    b.clear();
+    }
+
+
+return 0;
+}
