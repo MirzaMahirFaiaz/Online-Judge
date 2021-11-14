0fd59572de2cfdd1dc47eaf23f3615dde130c8ca
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-11-14 14:41:04 +0600

    Add files via upload

---
+++ Codeforces/1589 - A. Mathematical Addition.cpp
@@ -0,0 +1,26 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    unsigned long long t,i,j,u,v;
+    cin>>t;
+    while(t--){
+        cin>>u>>v;
+        u*=u;
+        v*=v;
+        cout<<u<<" -"<<v<<endl;
+    }
+
+
+
+return 0;
+}
