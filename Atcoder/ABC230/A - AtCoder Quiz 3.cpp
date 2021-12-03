beec442b050f44a466c34f692b2948f75c0878f8
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-12-03 19:25:19 +0600

    Create A - AtCoder Quiz 3.cpp

---
+++ Atcoder/ABC230/A - AtCoder Quiz 3.cpp
@@ -0,0 +1,27 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(NULL);
+
+    ll t,i,j;
+    cin>>i;
+    if(i<=41){
+        cout<<"AGC";
+        printf("%03d\n",i);
+
+    }
+    else{
+        cout<<"AGC";
+        printf("%03d\n",i+1);
+    }
+
+return 0;
+}
