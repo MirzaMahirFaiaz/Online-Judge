c43c01b942751306ab6cc7088b53227c4ba9ec19
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-12-14 01:04:19 +0600

    Add files via upload

---
+++ SPOJ/AE00 - Rectangles.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    int i,j,n,cnt=0;
+    scanf("%d",&n);
+
+
+    for(i=1;i<=sqrt(n);i++)
+        for(j=i+1;i*j<=n;j++)
+            cnt++;
+
+
+    cnt+=sqrt(n);
+    printf("%d",cnt);
+    return 0;
+}
