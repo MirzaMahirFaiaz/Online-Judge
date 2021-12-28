d29a6b690b2a7b55f0170cce4e296d71b96acfba
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2021-12-29 02:58:16 +0600

    Create 1582A - A. Luntik and Concerts.cpp

---
+++ Codeforces/1582A - A. Luntik and Concerts.cpp
@@ -0,0 +1,28 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j;
+    cin>>t;
+    while(t--){
+        ll a,b,c;
+        cin>>a>>b>>c;
+        j = a+(2*b)+(3*c);
+        if(j%2==1)
+            cout<<"1"<<endl;
+        else
+            cout<<"0"<<endl;
+
+    }
+
+return 0;
+}
