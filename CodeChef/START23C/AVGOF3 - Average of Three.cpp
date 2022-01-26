11371ccd0cd2ae4a74d5379c61b875667bef44d9
Mirza Mahir Faiaz <81585954+MirzaMahirFaiaz@users.noreply.github.com>
2022-01-27 02:54:35 +0600

    Create AVGOF3 - Average of Three.cpp

---
+++ CodeChef/START23C/AVGOF3 - Average of Three.cpp
@@ -0,0 +1,21 @@
+//@author MIRZA MAHIR FAIAZ, Dept. of CSE, AUST
+#include<stdio.h>
+#include<bits/stdc++.h>
+#define ll long long
+#define PI acos(-1)
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll t,i,j,x;
+    cin>>t;
+    while(t--){
+        cin >> x;
+		cout <<"1"<<" 2"<<" "<<x*3-3<<endl;
+    }
+return 0;
+}
